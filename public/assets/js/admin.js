af8ef211ecbb5facfd1398611fcf2aec71aa670a
thanhbinh <dtb751@gmail.com>
2021-12-14 00:18:17 +0700

    update admin

--- public/assets/js/admin.js
+++ public/assets/js/admin.js
@@ -1,19 +1,24 @@
-window.addEventListener("load", function() {
-    var addProduct = document.querySelector(".btn-admin");
-    var formAddProduct = document.querySelector(".container-form");
-    var opacity = this.document.querySelector(".container-opacity");
-    var exit = this.document.querySelector(".btn-exit");
+const menu = document.querySelectorAll(".menu-item");
+const tables = document.querySelectorAll(".table-item");
+const btns = document.querySelectorAll(".btn-add");
+const forms = document.querySelectorAll(".form-item")
+var exit = this.document.querySelector(".btn-exit");
+var opacity = this.document.querySelector(".container-opacity");
 
-    function showForms() {
-        formAddProduct.classList.add("show");
-        opacity.classList.add("show");
+console.log(forms);
+menu.forEach((item, index) => {
+    item.onclick = function(e) {
+        e.preventDefault();
+        document.querySelector(".table-item.active").classList.remove("active");
+        tables[index].classList.add("active");
     }
+    btns[index].onclick = function(e) {
+        forms[index].classList.add("active");
+        opacity.classList.add("show");
 
-    function exitForms() {
-        formAddProduct.classList.add("hide");
+    }
+    exit.onclick = function(e) {
+        forms[index].classList.remove("active");
         opacity.classList.add("hide");
     }
-
-    addProduct.addEventListener("click", showForms);
-    exit.addEventListener("click", exitForms);
 });
